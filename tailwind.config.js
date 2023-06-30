990d97cc6780fa300eb6be00917db3c4eebe539a
Ivan Zaburdaev <ivanlom227@gmail.com>
2023-06-30 08:46:46 +0300

    project setup, first block

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,11 +7,14 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontFamily:{
+        'raleway':['Raleway'],
+        'oranienbaum':['Oranienbaum'],
       },
+      colors:{
+        'gold':'#CFB26F',
+        'black':'#0F0F0F'
+      }
     },
   },
   plugins: [],
